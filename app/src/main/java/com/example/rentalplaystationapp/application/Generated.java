68160fe185632ec8776efb5904c4c2bb94cfe744
ryandhanielf <137640193+ryandhanielf@users.noreply.github.com>
2023-07-22 09:07:46 +0700

    pertama

--- app/src/main/java/com/example/rentalplaystationapp/application/Generated.java
+++
@@ -1,4 +0,0 @@
-package com.example.rentalplaystationapp.application;
-
-public @interface Generated {
-}
--- app/src/main/java/com/example/rentalplaystationapp/dao/Generated.java
+++
@@ -1,4 +0,0 @@
-package com.example.rentalplaystationapp.dao;
-
-public @interface Generated {
-}
